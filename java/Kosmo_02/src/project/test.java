6a0592c1a0e13ba82249a6621b25f346f6062b1d
reosong <ambition_1018@naver.com>
2021-12-28 16:47:46 +0900

    mvc2

---
+++ java/Kosmo_02/src/project/test.java
@@ -0,0 +1,17 @@
+package project;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		String a = "abc";
+		String aa = new String("abc");
+		
+		
+		System.out.println(a == aa);
+		System.out.println(a.equals(aa));
+		
+		
+		
+	}
+}
